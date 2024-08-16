7da716ddadf5205ecdaea9fc725c685c6463309b
Ana Vitória <anacvitoria00@gmail.com>
2024-08-16 20:01:44 -0300

    clothes seccions add

---
+++ scripts/index.js
@@ -0,0 +1,10 @@
+const stars = document.querySelectorAll('.star-icon');
+document.addEventListener('click',function(e){
+    const classStar = e.target.classList;
+    if(!classStar.contains('active')){
+       stars.forEach(function(star){
+           star.classList.remove('active');
+       })
+       classStar.add('active');
+       };
+    });
